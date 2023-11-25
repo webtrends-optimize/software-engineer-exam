6bd6fe3479f6036d0504367b81f5054884e2254d
Sandeep Shah <sandeep89@gmail.com>
2023-11-25 19:05:57 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+const fields = [
+    { name: "field_name", label: "Name", type: "text" },
+    { name: "field_username", label: "User", type: "text" },
+    { name: "field_date", label: "Date", type: "date" }
+];
+
+class App extends React.Component {
+
+    render(){
+
+        return (
+        
+<div>
+
+    <h1>Webtrends Optimize Software Engineer exam</h1>
+    <form></form>
+</div>
+            
+        );
+
+    }
+
+}
+
+export default App;
