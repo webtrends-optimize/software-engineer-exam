b868aefd20d4c169af9c05883c29c8cb276ecb0a
naman batra <namanbatra1927@gmail.com>
2024-01-10 17:13:10 +0530

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const fields = [
-    { name: "field_name", label: "Name", type: "text" },
-    { name: "field_username", label: "User", type: "text" },
-    { name: "field_date", label: "Date", type: "date" }
+  { name: "field_name", label: "Name", type: "text" },
+  { name: "field_username", label: "User", type: "text" },
+  { name: "field_date", label: "Date", type: "date" },
 ];
 
 class App extends React.Component {
-
-    render(){
-
-        return (
-        
-<div>
-
-    <h1>Webtrends Optimize Software Engineer exam</h1>
-    <form></form>
-</div>
-            
-        );
-
-    }
-
+  render() {
+    return (
+      <div>
+        <h1>Webtrends Optimize Software Engineer exam</h1>
+        <form></form>
+      </div>
+    );
+  }
 }
 
 export default App;
